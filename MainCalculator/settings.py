2d2221d8be99f912a36e7683447baaf5951f3791
Bakr <bakr22hj@gmail.com>
2024-03-31 00:39:01 -0400

    Change allowed hosts to the website hostname

--- MainCalculator/settings.py
+++ MainCalculator/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tbe-5l69ogamm94tnh7(hj=cc76)6w34l-4%a-@9n+36*49&au
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 
 
 # Application definition
